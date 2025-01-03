968a506b2fa1d34301b8e5d773283bdfd6552459
pawan2505 <pawanaktu@gmail.com>
2025-01-03 18:33:37 +0530

    JavaScript Introduction

---
+++ Introduction/script.js
@@ -0,0 +1,16 @@
+// console.log("Hello World");
+
+// alert("JavaScript Class");
+
+// document.write("Hey everyone!\n");
+
+
+// console.log(34+54);
+
+
+// let num = prompt("\nEnter number : ");
+
+// document.write("Number : "+num);
+
+console.log("Hey\n");
+console.log("How are you!");

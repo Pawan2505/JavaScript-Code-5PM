1a5bf582b0409b4c387b82aa8aca6e9fe3c8cdd1
pawan2505 <pawanaktu@gmail.com>
2025-01-06 18:44:16 +0530

    datatype keyword

---
+++ OUTPUT SHOW METHOD/script.js
@@ -0,0 +1,103 @@
+
+/*var fistname = "Pawan Maurya";
+
+console.log(fistname);
+
+fistname = "pranav";
+
+console.log(fistname);
+
+// let 123num = 10;  // not ok
+// let num123 = 10;
+// console.log(num123);
+// let first name;   // not ok
+let first_name = "Pawan";
+
+
+console.log(first_name);*/
+
+// let $num1 = 10;
+
+// console.log($num1);
+
+// let @num1 = 10;  // not ok
+
+// console.log(@num1);
+
+// let num = 12;
+
+// let Num = 20;
+
+// console.log(num);
+// console.log(Num);
+
+
+// let if = 10;  // not ok -> keywoard not allowed
+
+
+//  Javascript keywoard: https://www.w3schools.com/js/js_reserved.asp
+
+
+// var , let , const
+
+
+// var num = 10;
+
+// console.log(num);
+
+// var num = 20;
+
+// console.log(num);
+
+
+// let x = 10;
+
+// console.log(x);
+
+
+// let x = 20; // Not ok
+
+
+
+// block of code (scope)
+
+
+// let y = 10;
+
+// console.log(y);  //10
+
+// {
+//     let y = 20;
+//     console.log(y);  //20
+// }
+
+// console.log(y); //10
+
+
+
+// {
+//     let z = 30;
+//     console.log(z);  //30
+// }
+
+// console.log(z); // undefine
+
+// {
+    
+// let a = 50; // global variable
+
+// {
+//     let b = 30; // local variable
+//     console.log(a); // 50
+//     console.log(b); // 30
+// }
+
+// console.log(a); //50
+// }
+
+// gloable variable can access any block scope
+
+
+// value = 40; // it is not good practice -> good practice is used datatype  keyword
+
+// console.log(value);

ac2cc3acf1a307f3c2afb83e4b7535f4b7e841b6
pawan2505 <pawanaktu@gmail.com>
2025-01-16 18:36:05 +0530

    comparision and logial operator

---
+++ Operator/code/comprision.js
@@ -0,0 +1,16 @@
+let pranav_age = parseInt(prompt("Enter Age : "));
+let saytam_age = prompt("Enter Age : ");
+
+console.log("pranav_age : ",pranav_age);
+console.log("saytam_age : ",saytam_age);
+
+console.log(`pranav_age>saytam_age : ${pranav_age>saytam_age}`);
+console.log("pranav_age<saytam_age"+pranav_age<saytam_age);
+
+/*console.log("pranav_age<=saytam_age"+pranav_age<=saytam_age);
+console.log("pranav_age>=saytam_age"+pranav_age>=saytam_age);
+console.log("pranav_age===saytam_age"+pranav_age===saytam_age);
+console.log("pranav_age==saytam_age"+pranav_age==saytam_age);
+console.log("pranav_age!=saytam_age"+pranav_age!=saytam_age);
+console.log("pranav_age!==saytam_age"+pranav_age!==saytam_age);*/
+
---
+++ Operator/code/swapping.js
@@ -0,0 +1,11 @@
+let num1 = 10;
+let num2 = 20;
+
+let num3 = num1;
+
+num1 = num2;
+num2 = num3;
+
+
+console.log(`Num1 : ${num1}`);
+console.log(`Num2 : ${num2}`);
